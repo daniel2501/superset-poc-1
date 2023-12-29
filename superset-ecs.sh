e887fbe3e3ccec856c2ff305559d39b14d0c63c8
Daniel Benedict <daniel2501@gmail.com>
2023-12-28 22:02:20 -0500

    Pulls the official Apache Superset docker image, pushes it to ECR, provisions a cluster, task definitions, IAM roles, and runs it on ECS.

---
+++ superset-ecs.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+docker pull apache/superset
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+source ~/.zshrc
+export AWS_ACCESS_KEY_ID=*********************
+export AWS_SECRET_ACCESS_KEY=*********************************
+export AWS_DEFAULT_REGION=us-east-1
+# Create ECR repository
+aws ecr get-login-password
+aws ecr | docker login -u AWS -p $(aws ecr get-login-password) \
+    128862924679.dkr.ecr.us-east-1.amazonaws.com/superset-poc-1
+aws ecs register-task-definition \
+    --cli-input-json file://$HOME/repos/superset-oc-1/task-definition.json
+aws ecs create-service --cluster superset-poc-cluster-1 \
+    --service-name superset-poc-service-1 \
+    --task-definition superset-ci:6 --desired-count 1 \
+    --launch-type "FARGATE" \
+    --network-configuration "awsvpcConfiguration={subnets=[subnet-0dd76b52],securityGroups=[sg-39833e09], assignPublicIp=ENABLED}" \
+    --enable-execute-command
+aws iam create-role --role-name ecsTaskExecutionRole \
+    --assume-role-policy-document file://ecs-tasks-trust-policy.json
+aws ecs run-task --cluster arn:aws:ecs:us-east-1:128862924679:cluster/superset-poc-cluster-1 \
+    --task-definition arn:aws:ecs:us-east-1:128862924679:task-definition/superset-ci:7 \
+    --launch-type FARGATE --count 1 \
+    --network-configuration "awsvpcConfiguration={subnets=[subnet-0dd76b52],securityGroups=[sg-39833e09], assignPublicIp=ENABLED}"
